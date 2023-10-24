fe951406a1e587c0f93654fd6205782e8757282d
Jeremy Ambrosio <53021011+jeremyambrosio@users.noreply.github.com>
2023-10-24 15:37:47 -0500

    Update streamlit_app.py
    added input box

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-
+# Adding input section
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 # New Section to display fruityvice api response
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +"kiwi")
