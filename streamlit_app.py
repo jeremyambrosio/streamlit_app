0dd8c10379c544e897890600cd9600a132985b77
Jeremy Ambrosio <53021011+jeremyambrosio@users.noreply.github.com>
2023-10-24 16:30:19 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -9,7 +13,8 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-import pandas
+
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 # Set the index to be the fruit names
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -28,8 +33,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 # Adding input section
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-# New Section to display fruityvice api response
-import requests
+
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
 # take the json version of he response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
@@ -39,9 +44,7 @@ streamlit.dataframe(fruityvice_normalized)
 # don't run anything past here while we troubleshoot
 streamlit.stop()
 
-# The line shown below will tell your app to bring in some code from the snowflake library you added (snowflake-connector-python). 
-import snowflake.connector
-
+#import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
