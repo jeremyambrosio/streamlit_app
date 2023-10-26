b6d6523b6e5e40868ec63a46b48a10ee219f3647
Jeremy Ambrosio <53021011+jeremyambrosio@users.noreply.github.com>
2023-10-26 14:18:19 -0500

    Update streamlit_app.py
    cleaned up and added error handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,16 +30,24 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-# Adding input section
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+# Adding input section and try/except error handling
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+      #import requests
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+      # take the json version of he response and normalize it
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      # output the screen as a table
+      streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
+
 streamlit.write('The user entered ', fruit_choice)
 
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
-# take the json version of he response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output the screen as a table
-streamlit.dataframe(fruityvice_normalized)
+
 
 # don't run anything past here while we troubleshoot
 streamlit.stop()
